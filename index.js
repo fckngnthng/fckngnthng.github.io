eb091d180bbebe6a482d4eb769996a4adbd9072a
Юрий Голиков <y.golikov@isimplelab.com>
2021-05-19 17:24:56 +0300

    test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ console.log('index.js init ver16');
 
 function atbClick(link) {
   console.log(link);
-  window.parent.postMessage(link, '*');
+  window.parent.postMessage(JSON.stringify({ event: 'ATBExtLink', link }), '*');
 
 }

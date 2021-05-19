692bda79541f439316fe4573129d405a0f727a89
Юрий Голиков <y.golikov@isimplelab.com>
2021-05-19 15:32:39 +0300

    test

--- index.js
+++ index.js
@@ -5,11 +5,14 @@
 
   document.helper = document.createElement('div');
 
+  document.generateEventClick = generateEventClick;
+
+  function generateEventClick(link) {
+    console.log('ATBHrefClick', link)
+    console.log(document.helper);
+    document.helper.dispatchEvent(new Event('ATBHrefClick', { link }))
+  }
+
 })()
 
-function generateEventClick(link) {
-  console.log('ATBHrefClick', link)
-  console.log(document.helper);
-  document.helper.dispatchEvent(new Event('ATBHrefClick', { link }))
-}
 

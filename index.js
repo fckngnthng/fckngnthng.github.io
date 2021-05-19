23e354f69e0c832db7e48f752aac1c4518b1f5e0
Юрий Голиков <y.golikov@isimplelab.com>
2021-05-19 15:54:25 +0300

    test

--- index.js
+++ index.js
@@ -6,6 +6,14 @@
   document.addEventListener('iframeLoad', ev => {
     var iframe = document.getElementById('test-external-iframe');
     console.log(iframe)
+    const externalLinks = (externalIframe.contentDocument || contentWindow.document).body.querySelectorAll('a')
+    console.log('externalLinks')
+    externalLinks.forEach(element => {
+        element.addEventListener('click', function (e) {
+            e.preventDefault()
+            console.log(this.href)
+        })
+    })
   })
 
 })()

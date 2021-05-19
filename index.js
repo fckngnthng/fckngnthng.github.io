97068d3e4278d6233f04f53b92ab26ee3e458353
Юрий Голиков <y.golikov@isimplelab.com>
2021-05-19 17:21:23 +0300

    test

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-
-// (function () {
-
-  console.log('index.js init 15');
-
-//   // document.addEventListener('iframeLoad', ev => {
-//   //   var iframe = document.getElementById('test-external-iframe');
-//   //   console.log(iframe)
-//   //   const externalLinks = (iframe.contentDocument || iframe.contentWindow.document).body.querySelectorAll('a')
-//   //   console.log(externalLinks);
-//   //   externalLinks.forEach(element => {
-//   //       element.addEventListener('click', function (e) {
-//   //           e.preventDefault()
-//   //           console.log(this.href)
-//   //       })
-//   //   })
-//   // })
-
-//   // var frame = document.getElementById('new-atb-product-iframe');
-//   // var frameWindow = frame.contentWindow ? frame.contentWindow : frame.contentDocument.defaultView;
-
-//   // const event = new CustomEvent({ link })
-//   // frameWindow.postMessage(JSON.stringify(event), '*');
-
-// })()
+console.log('index.js init ver15');
 
 function atbClick(link) {
-  // var frame = document.getElementById('new-atb-product-iframe');
-  // var frameWindow = frame.contentWindow ? frame.contentWindow : frame.contentDocument.defaultView;
-
+  console.log(link);
   const event = new CustomEvent({ link })
-  window.parent.postMessage(JSON.stringify(event), '*');
+  window.parent.postMessage(link, '*');
 
 }

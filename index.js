59893adc5070c6e8c3f9e87261d102129e4e89ac
fckngnthng <ruglaz@gmail.com>
2021-05-24 18:04:04 +0300

    update post message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function atbBack() {
   console.log(window.history);
   if (window.history.length === 2) {
-    window.parent.postMessage(JSON.stringify({ type: 'ATBClosePage' }), '*');
+    window.postMessage(JSON.stringify({ type: 'ATBClosePage' }), '*');
   } else window.history.back();
 }

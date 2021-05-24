c5632875f709f4d1547a705b132bf64d423373b1
fckngnthng <ruglaz@gmail.com>
2021-05-24 18:13:23 +0300

    update message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function atbBack() {
   console.log(window.history);
-  if (window.history.length === 2) {
-    window.postMessage(JSON.stringify({ type: 'ATBClosePage' }), '*');
+  if (window.history.length === 1) {
+    window.webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify({ type: 'ATBClosePage' }))
   } else window.history.back();
 }
 

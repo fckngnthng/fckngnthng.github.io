39d90adb4abcc3162cf83f08f54737f0bbdbd439
fckngnthng <ruglaz@gmail.com>
2021-05-24 18:19:23 +0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function atbBack() {
   console.log(window.history);
-  if (window.history.length === 1) {
+  if (window.history.length === 2) {
+    console.log('pm');
     window.webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify({ type: 'ATBClosePage' }))
   } else window.history.back();
 }
 
-console.log('version 2');
+console.log('version 3');

9fd0fae1166a236842844a8d10ba89f6ed2517eb
Юрий Голиков <y.golikov@isimplelab.com>
2021-05-24 17:55:37 +0300

    Add close window code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function atbBack() {
   console.log(window.history);
-  window.history.back();
+  if (window.history.length === 2) {
+    window.parent.postMessage(JSON.stringify({ type: 'ATBClosePage' }), '*');
+  } else window.history.back();
 }
